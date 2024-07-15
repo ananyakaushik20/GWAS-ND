650aab582604b3573f947b12b2bfe02633c0182a
ananyakaushik20 <85845284+ananyakaushik20@users.noreply.github.com>
2024-07-15 21:24:41 +0100

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,6 @@ df = analys.get_data('mhat').data
 
 # Display the first few rows of the dataframe
 print(df.head())
-# Note, an internal function maps the SNP ID to chromsome and base pair positions. 
 
 visuz.marker.mhat(df=df, chr='chr',pv='pvalue', show=True) # plot chr on the X axis and -log10(p-value)on the Y axis that gets calculated automatically
 visuz.marker.mhat(df=df, chr='chr',pv='pvalue', color=("#d7d1c9", "#696464"), gwas_sign_line=True, gwasp=5E-08, 
